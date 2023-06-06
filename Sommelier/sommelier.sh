56940102dfd2ffd4caa1c68d4d8ec17466b7eb82
YakupAltay <yakup.altay@boun.edu.tr>
2023-06-07 01:53:42 +0300

    Update sommelier.sh

--- Sommelier/sommelier.sh
+++ Sommelier/sommelier.sh
@@ -53,10 +53,12 @@ if [ ! $MONIKER ]; then
 	echo 'export MONIKER='$MONIKER >> $HOME/.bash_profile
 fi
 
-
+echo "5 installation_progress"
 
 # Updates
 sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y && sudo apt install make clang pkg-config libssl-dev build-essential git jq ncdu bsdmainutils htop net-tools lsof -y < "/dev/null" && sudo apt-get update -y && sudo apt-get install wget liblz4-tool aria2 -y && sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu -y
+
+echo "35 installation_progress"
 
 cd $HOME
 wget "https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz"
@@ -67,6 +69,7 @@ echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 go version
 
+echo "55 installation_progress"
 
 sleep 1
 
@@ -81,6 +84,7 @@ sleep 1
 
 $EXECUTE init $MONIKER --chain-id $CHAIN_ID
 
+echo "70 installation_progress"
 
 # Set peers and seeds
 sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/$SYSTEM_FOLDER/config/config.toml
@@ -125,11 +129,13 @@ EOF
 SNAPSHOT=https://snapshots.polkachu.com/snapshots/sommelier/sommelier_8894470.tar.lz4
 curl -L $SNAPSHOT | tar -Ilz4 -xf - -C $HOME/$SYSTEM_FOLDER
 
-
+echo "90 installation_progress"
 
 sudo systemctl daemon-reload
 sudo systemctl enable $EXECUTE
 sudo systemctl restart $EXECUTE
+
+echo "export NODE_PROPERLY_INSTALLED=true" >> $HOME/.bash_profile
 
 echo '=============== SETUP IS FINISHED ==================='
 echo -e "CHECK OUT YOUR LOGS : \e[1m\e[32mjournalctl -fu ${EXECUTE} -o cat\e[0m"
