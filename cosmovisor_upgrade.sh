4cfc1db8e18fd5f956d5a29f39dfd58cbe712780
aytunc <109796545+saitunc@users.noreply.github.com>
2023-06-14 16:38:02 +0300

    Cosmovisor test#1

---
+++ cosmovisor_upgrade.sh
@@ -0,0 +1,42 @@
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@latest
+
+echo "export DAEMON_NAME=${EXECUTE}" >> $HOME/.bash_profile
+echo "export DAEMON_HOME=${SYSTEM_FOLDER}" >> $HOME/.bash_profile
+echo "export DAEMON_ALLOW_DOWNLOAD_BINARIES=true" >> $HOME/.bash_profile
+echo "export DAEMON_RESTART_AFTER_UPGRADE=true" >> $HOME/.bash_profile
+echo "export UNSAFE_SKIP_BACKUP=true" >> $HOME/.bash_profile
+
+source $HOME/.bash_profile
+
+mkdir -p $DAEMON_HOME/cosmovisor/genesis/bin && mkdir -p $DAEMON_HOME/cosmovisor/upgrades
+
+cp $HOME/go/bin/$DAEMON_NAME $DAEMON_HOME/cosmovisor/genesis/bin
+
+cosmovisor init $HOME/go/bin/$DAEMON_NAME
+
+
+sudo tee <<EOF >/dev/null /etc/systemd/system/$EXECUTE.service
+[Unit]
+Description=$EXECUTE
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which $EXECUTE) start --home $HOME/$SYSTEM_FOLDER
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo -S systemctl daemon-reload
+
+sudo -S systemctl enable cosmovisor
+
+sudo systemctl start cosmovisor
+
+sudo systemctl status cosmovisor
+
+sudo journalctl -u cosmovisor -f
