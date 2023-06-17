3f091a1d31c83265dc637996f363c1ceb4aea9f3
YakupAltay <yakup.altay@boun.edu.tr>
2023-06-18 00:16:13 +0300

    Works but needs to have a dynamic snapshot

--- Iris/iris.sh
+++ Iris/iris.sh
@@ -13,20 +13,20 @@ echo -e ':::   ::   ::::::   :::::::   :::::::     :::   ::::::::     :::'
 echo -e '\e[0m'
 
 # Variables
-
+EVM_CHAIN_ID=6688
 EXECUTE=iris
 CHAIN_ID=irishub-1
 SYSTEM_FOLDER=.iris
 PROJECT_FOLDER=irishub
-VERSION=v2.0.0
-REPO=https://github.com/irisnet/irishub
+VERSION=v2.0.3
+REPO=https://github.com/irisnet/irishub.git
 GENESIS_FILE=https://raw.githubusercontent.com/irisnet/mainnet/master/config/genesis.json
-ADDRBOOK=
+ADDRBOOK=https://api-minio-nord.imperator.co/addrbook/iris/addrbook.json
 PORT=26
-DENOM=iris
+DENOM=uiris
 GO_VERSION=$(curl -L https://golang.org/VERSION?m=text | sed 's/^go//')
-PEERS="fdc0406afdd3acc63f74f5439e09104f663a7c1f@44.241.177.178:26656,090bcbe5302e6104821a96c4899912870db04cb9@52.11.128.123:26656,83b3f989f3ce089afdf733f8aa06e792d7e00c08@3.34.6.30:26656,9cd1fe0109d347d49976a7353f8adcd308211f08@irisapi.terminet.io:26656,87f18756b93d835c59fe5ce2a8da51858837eb5b@54.180.15.28:26656,90e48220190b16cad95145b6213d512d703e5617@138.197.158.189:26656,7fad2da10c41b0c1e3c2ce6e708f7fa817b5e19d@135.181.56.26:46656,ebfb43ca1b592b5f8a1faf3e2aa1a34e8e1099cc@iris01.dokia.cloud:26656,a17d7923293203c64ba75723db4d5f28e642f469@seed-2.mainnet.irisnet.org:26656,fbaf634a899c7aab3c159ce1a345122bbeca3717@209.133.200.154:26656,92fadc989ed29aee0d46afce3226f8565d1f36cb@144.91.116.17:46656,895d5a5009d042108783a6aeb0991c5186a46617@144.76.96.47:26656,40821d0ade83d582b29d748f37ecf7bef0a823d0@66.42.116.195:26656,f4af63df2f5c63428be776e56ffc2899fa47afdf@47.101.160.78:26656,08e2f9453541b104df84efa68ab2f0d242eb829b@176.9.47.69:26656,db3dacf404840e067b2f59e304cb2b6662ec0cea@173.212.212.252:26656,87525da8c830da2c3a861638a77f601278efd353@185.181.103.142:26656,4ac6200974d3fd80a8e49d145a2c254ed37a9b9a@159.69.106.156:26656,4e02a4b4f4350ea2b770cd03dc41fedcadb13176@159.69.55.206:26656,5d659b3edde90344489210f5be90c0682b54b997@45.32.72.122:16656,63f5646b5f9ce927241383a091b60f797796588f@143.110.240.198:26656,84cc32adca3986b35953886ad075431d318a98b5@52.214.130.28:46656,5387e8f13cee43b47526d2d6a9d8ee65b14e4b8f@161.97.187.139:26656"
-SEEDS=
+PEERS="b037e8fd1598a37a4773fa4b71ccdd2ae8d0b186@39.105.215.48:26656,107448aea4609790c7b710c03d29b70bfa7e8c9f@5.9.99.172:22656,fcc1d9bc43163b20ce5163cf41a302b33f25375f@34.82.96.8:26656,fca15d1a704507487c6d094ae67e56156253a735@13.113.233.138:26656,363430f2533f0b284c156e846b6934bb7e09ea82@35.221.235.168:26656,35d8743585e09151ada67003c43ac2b0cee36d0f@5.161.68.228:26656,b212d5740b2e11e54f56b072dc13b6134650cfb5@134.65.192.157:26656,68a5dbab2686566290c5431f203934c628d6e5b3@185.16.38.234:26656,3c59671586a77c3a81e3e2c8052a8a578a6de5f8@195.201.63.87:37666"
+SEEDS="6a6de770deaa4b8c061dffd82e9c7f4d40c2165d@seed-1.mainnet.irisnet.org:26656,a17d7923293203c64ba75723db4d5f28e642f469@seed-2.mainnet.irisnet.org:26656,e1b058e5cfa2b836ddaa496b10911da62dcf182e@iris-seed-1.allnodes.me:26656,445b38a181d147c243185d94567412e5c5f1a22c@seed-irisnet-01.stakeflow.io:1906"
 
 sleep 2
 
@@ -53,13 +53,12 @@ if [ ! $MONIKER ]; then
 	echo 'export MONIKER='$MONIKER >> $HOME/.bash_profile
 fi
 
-echo "5 installation_progress"
 
 # Updates
 sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y && sudo apt install make clang pkg-config libssl-dev build-essential git jq ncdu bsdmainutils htop net-tools lsof -y < "/dev/null" && sudo apt-get update -y && sudo apt-get install wget liblz4-tool aria2 -y && sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu -y
 
-echo "35 installation_progress"
 
+# Go installation
 cd $HOME
 wget "https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
@@ -69,7 +68,6 @@ echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 go version
 
-echo "55 installation_progress"
 
 sleep 1
 
@@ -78,24 +76,40 @@ rm -rf $PROJECT_FOLDER
 git clone $REPO
 cd $PROJECT_FOLDER
 git checkout $VERSION
+make build
 make install
 sleep 1
 
-
+$EXECUTE config chain-id $CHAIN_ID
+$EXECUTE config keyring-backend test
+$EXECUTE config node tcp://localhost:15557
 $EXECUTE init $MONIKER --chain-id $CHAIN_ID
 
-$EXECUTE tendermint unsafe-reset-all
 
-
-# Download genesis and config.toml
-CONFIG_FILE=https://raw.githubusercontent.com/irisnet/mainnet/master/config/config.toml
-curl -o $HOME/$SYSTEM_FOLDER/config/config.toml $CONFIG_FILE
-curl -Ls $GENESIS_FILE > $HOME/$SYSTEM_FOLDER/config/genesis.json
-
-# Set peers
+# Set peers and seeds
 sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|" $HOME/$SYSTEM_FOLDER/config/config.toml
+sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/$SYSTEM_FOLDER/config/config.toml
+sed -i -e "s|^evm_chain_id *=.*|evm_chain_id = \"$EVM_CHAIN_ID\"|" $HOME/$SYSTEM_FOLDER/config/config.toml
 
-echo "80 installation_progress"
+# Download genesis and addrbook
+curl -Ls $GENESIS_FILE > $HOME/$SYSTEM_FOLDER/config/genesis.json
+curl -Ls $ADDRBOOK > $HOME/$SYSTEM_FOLDER/config/addrbook.json
+
+
+# Set Config Pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="10"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+
+
+# Set minimum gas price
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0$DENOM\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+
 
 # Creating your systemd service
 sudo tee <<EOF >/dev/null /etc/systemd/system/$EXECUTE.service
@@ -114,18 +128,20 @@ LimitNOFILE=65535
 WantedBy=multi-user.target
 EOF
 
+sleep 3 
 
-#state sync
-SNAPSHOT=https://storage.googleapis.com/irishub-1/irishub-data-v2.0.0.tgz
-curl -L $SNAPSHOT | tar -Ilz4 -xf - -C $HOME/$SYSTEM_FOLDER
+#fast sync with snapshot
+SNAPSHOT=https://api-minio-sud.imperator.co/snapshots/iris/iris_20360127.tar.lz4
+cp $HOME/$SYSTEM_FOLDER/data/priv_validator_state.json $HOME/$SYSTEM_FOLDER/priv_validator_state.json.backup
+rm -rf $HOME/$SYSTEM_FOLDER/data/*
+mv $HOME/$SYSTEM_FOLDER/priv_validator_state.json.backup $HOME/$SYSTEM_FOLDER/data/priv_validator_state.json
+curl -L $SNAPSHOT | tar -I lz4 -xf - -C $HOME/$SYSTEM_FOLDER
 
-echo "90 installation_progress"
 
 sudo systemctl daemon-reload
 sudo systemctl enable $EXECUTE
 sudo systemctl restart $EXECUTE
 
-echo "export NODE_PROPERLY_INSTALLED=true" >> $HOME/.bash_profile
 
 echo '=============== SETUP IS FINISHED ==================='
 echo -e "CHECK OUT YOUR LOGS : \e[1m\e[32mjournalctl -fu ${EXECUTE} -o cat\e[0m"
