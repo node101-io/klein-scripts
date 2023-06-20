4e7657cf8f84f1d9bac83f0c9107797de108fbbc
Necip Baha Sağıroğlu <114649879+itu-itis22-sagiroglun22@users.noreply.github.com>
2023-06-20 13:46:51 +0300

    Delete Testnet directory

--- Testnet/Babylon/reset.sh
+++
@@ -1,7 +0,0 @@
-sudo systemctl stop babylond
-sudo systemctl disable babylond
-sudo rm /etc/systemd/system/babylon* -rf
-sudo rm $(which babylond) -rf
-sudo rm $HOME/.babylond* -rf
-sudo rm $HOME/babylon -rf
-sed -i '/BABYLON_/d' ~/.bash_profile
--- Testnet/Nibiru/reset_nibiru.sh
+++
@@ -1,7 +0,0 @@
-sudo systemctl stop nibid
-sudo systemctl disable nibid
-sudo rm /etc/systemd/system/nibi* -rf
-sudo rm $(which nibid) -rf
-sudo rm $HOME/.nibid* -rf
-sudo rm $HOME/nibiru -rf
-sed -i '/NIBIRU_/d' ~/.bash_profile
