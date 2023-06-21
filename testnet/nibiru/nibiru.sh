68ca58f3f39d69ae21a6c7c93cc0862df7db413f
YakupAltay <yakup.altay@boun.edu.tr>
2023-06-21 22:02:23 +0300

    nibiru.sh updated

--- testnet/nibiru/nibiru.sh
+++ testnet/nibiru/nibiru.sh
@@ -13,131 +13,131 @@ echo -e ':::   ::   ::::::   :::::::   :::::::     :::   ::::::::     :::'
 echo -e '\e[0m'
 
 # Variables
-
-EXECUTE="nibid"
-CHAIN_ID="nibiru-itn-1"
-PORT=26
+EXECUTE=nibid
+CHAIN_ID=nibiru-itn-1	
 SYSTEM_FOLDER=.nibid
-PROJECT_FOLDER="nibiru"
-VERSION=v0.19.2
+PROJECT_FOLDER=nibiru
+VERSION=v0.19.2	
+REPO=https://github.com/NibiruChain/nibiru.git
+GENESIS_FILE=https://snapshots.kjnodes.com/nibiru-testnet/genesis.json
+ADDRBOOK=https://snapshots.kjnodes.com/nibiru-testnet/addrbook.json
+PORT=26
+DENOM=unibi
 GO_VERSION=$(curl -L https://golang.org/VERSION?m=text | sed 's/^go//')
-REPO="https://github.com/NibiruChain/nibiru.git"
-GENESIS_FILE="https://raw.githubusercontent.com/Pa1amar/testnets/main/nibiru/nibiru-itn-1/genesis.json"
-SEEDS="3f472746f46493309650e5a033076689996c8881@nibiru-testnet.rpc.kjnodes.com:39659,a431d3d1b451629a21799963d9eb10d83e261d2c@seed-1.itn-1.nibiru.fi:26656,6a78a2a5f19c93661a493ecbe69afc72b5c54117@seed-2.itn-1.nibiru.fi:26656"
-PEERS=""
-ADDRBOOK="https://snapshots2-testnet.nodejumper.io/nibiru-testnet/addrbook.json"
-DENOM="unibi"
+PEERS="e2ad22b7cefbddd747c29d90882561e566ff2d3e@65.109.50.106:26656,74405e27923c1efe97fc678aa9f0357537a9b311@161.97.64.38:26656,b3a2a298c6a84c503253d120e3eee0c54cea90fd@137.184.193.235:20356,b9f203a7d45a2a2766ff144ea9cc680987886772@85.239.242.186:26656,bd4e84bd7b14201661c958c6cb6a1de2a27078ed@95.217.156.62:26656,0faa013496da308cf091099bb736f512f17ab380@185.144.99.55:26656,a03eaa525bd984d713fd9b000a89163dc7516a83@185.207.250.222:26656,5160aa16c18a37d25c3e667c80de83279715f2aa@195.2.75.252:26656,d5519e378247dfb61dfe90652d1fe3e2b3005a5b@65.109.68.190:13956,ce2dcfe5794bed1d4b32b8a43b32afc5d5e435f2@135.181.114.98:46656,f98a8229e5dc6da6d5e49fd4e115472df3d1773c@95.9.36.100:26656,b253cc6155ec59ea623f3f453d2f5a4b9c6d08fc@212.15.59.91:39656"
+SEEDS="3f472746f46493309650e5a033076689996c8881@nibiru-testnet.rpc.kjnodes.com:13959"
 
 sleep 2
+
 echo "export EXECUTE=${EXECUTE}" >> $HOME/.bash_profile
 echo "export CHAIN_ID=${CHAIN_ID}" >> $HOME/.bash_profile
-echo "export PORT=${PORT}" >> $HOME/.bash_profile
 echo "export SYSTEM_FOLDER=${SYSTEM_FOLDER}" >> $HOME/.bash_profile
 echo "export PROJECT_FOLDER=${PROJECT_FOLDER}" >> $HOME/.bash_profile
 echo "export VERSION=${VERSION}" >> $HOME/.bash_profile
-echo "export GO_VERSION=${GO_VERSION}" >> $HOME/.bash_profile
 echo "export REPO=${REPO}" >> $HOME/.bash_profile
 echo "export GENESIS_FILE=${GENESIS_FILE}" >> $HOME/.bash_profile
-echo "export SEEDS=${SEEDS}" >> $HOME/.bash_profile
-echo "export PEERS=${PEERS}" >> $HOME/.bash_profile
 echo "export ADDRBOOK=${ADDRBOOK}" >> $HOME/.bash_profile
+echo "export PORT=${PORT}" >> $HOME/.bash_profile
 echo "export DENOM=${DENOM}" >> $HOME/.bash_profile
+echo "export GO_VERSION=${GO_VERSION}" >> $HOME/.bash_profile
+echo "export PEERS=${PEERS}" >> $HOME/.bash_profile
+echo "export SEEDS=${SEEDS}" >> $HOME/.bash_profile
+
+
 source $HOME/.bash_profile
 
 sleep 1
-
 if [ ! $MONIKER ]; then
 	read -p "ENTER MONIKER NAME: " MONIKER
 	echo 'export MONIKER='$MONIKER >> $HOME/.bash_profile
 fi
 
+echo "5 installation_progress"
 
-#Dependencies
+# Updates
+sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y && sudo apt install make clang pkg-config libssl-dev build-essential git jq ncdu bsdmainutils htop net-tools lsof -y < "/dev/null" && sudo apt-get update -y && sudo apt-get install wget liblz4-tool aria2 -y && sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu -y
 
-cd $HOME
-sudo apt update
-sudo apt install lz4
-sudo apt install make clang pkg-config libssl-dev build-essential git jq ncdu bsdmainutils htop -y < "/dev/null"
+echo "30 installation_progress"
 
-echo "10 installation_progress"
-
-#Installation of GoLang
-
+# Go installation
 cd $HOME
 wget "https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf "go$GO_VERSION.linux-amd64.tar.gz"
 rm "go$GO_VERSION.linux-amd64.tar.gz"
-
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 go version
 
-echo "45 installation_progress"
-
+echo "60 installation_progress"
 
 sleep 1
+
+cd $HOME
+rm -rf $PROJECT_FOLDER
 git clone $REPO
 cd $PROJECT_FOLDER
 git checkout $VERSION
+make build
 make install
-nibid version # v0.19.2
+sleep 1
 
 $EXECUTE config chain-id $CHAIN_ID
 $EXECUTE config keyring-backend test
 $EXECUTE config node tcp://localhost:26657
 $EXECUTE init $MONIKER --chain-id $CHAIN_ID
 
-curl -s $ADDRBOOK > $HOME/$SYSTEM_FOLDER/config/addrbook.json
-wget $GENESIS_FILE -O $SYSTEM_FOLDER/config/genesis.json
 
+# Set peers and seeds
+sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|" $HOME/$SYSTEM_FOLDER/config/config.toml
+sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/$SYSTEM_FOLDER/config/config.toml
 
+# Download genesis and addrbook
+curl -Ls $GENESIS_FILE > $HOME/$SYSTEM_FOLDER/config/genesis.json
+curl -Ls $ADDRBOOK > $HOME/$SYSTEM_FOLDER/config/addrbook.json
 
-#Set configuration settings
+echo "85 installation_progress"
 
-sed -i 's|seeds =.*|seeds = "'$(curl -s https://networks.itn.nibiru.fi/$CHAIN_ID/seeds)'"|g' $SYSTEM_FOLDER/config/config.toml
+# Set Config Pruning
 pruning="custom"
 pruning_keep_recent="100"
 pruning_keep_every="0"
 pruning_interval="10"
-#sed -i 's|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $SYSTEM_FOLDER/config/config.toml
-sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $SYSTEM_FOLDER/config/app.toml
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $SYSTEM_FOLDER/config/app.toml
-sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $SYSTEM_FOLDER/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $SYSTEM_FOLDER/config/app.toml
-sed -i 's|^snapshot-interval *=.*|snapshot-interval = 0|g' $HOME/$SYSTEM_FOLDER/config/app.toml
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
 
-sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.025unibi\"/" $SYSTEM_FOLDER/config/app.toml
-sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/$SYSTEM_FOLDER/config/config.toml
 
-# sed -i 's|enable =.*|enable = true|g' $SYSTEM_FOLDER/config/config.toml
-# sed -i 's|rpc_servers =.*|rpc_servers = "'$(curl -s https://networks.itn.nibiru.fi/$CHAIN_ID/rpc_servers)'"|g' $SYSTEM_FOLDER/config/config.toml
-# sed -i 's|trust_height =.*|trust_height = "'$(curl -s https://networks.itn.nibiru.fi/$CHAIN_ID/trust_height)'"|g' $SYSTEM_FOLDER/config/config.toml
-# sed -i 's|trust_hash =.*|trust_hash = "'$(curl -s https://networks.itn.nibiru.fi/$CHAIN_ID/trust_hash)'"|g' $SYSTEM_FOLDER/config/config.toml
+# Set minimum gas price
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.025$DENOM\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
 
-echo "70 installation_progress"
 
 # Creating your systemd service
-sudo tee /etc/systemd/system/nibid.service > /dev/null <<EOF
+sudo tee <<EOF >/dev/null /etc/systemd/system/$EXECUTE.service
 [Unit]
-Description=NIBIRU Node
-After=network.target
+Description=$EXECUTE
+After=network-online.target
 
 [Service]
 User=$USER
-Type=simple
-ExecStart=$(which nibid) start
+ExecStart=$(which $EXECUTE) start --home $HOME/$SYSTEM_FOLDER
 Restart=on-failure
+RestartSec=10
 LimitNOFILE=65535
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-$EXECUTE tendermint unsafe-reset-all --home $SYSTEM_FOLDER --keep-addr-book
+sleep 3 
 
-SNAP_NAME=$(curl -s https://snapshots2-testnet.nodejumper.io/nibiru-testnet/info.json | jq -r .fileName)
-curl "https://snapshots2-testnet.nodejumper.io/nibiru-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C $HOME/$SYSTEM_FOLDER
+#fast sync with snapshot
+SNAPSHOT=https://snapshots.kjnodes.com/nibiru-testnet/snapshot_latest.tar.lz4
+cp $HOME/$SYSTEM_FOLDER/data/priv_validator_state.json $HOME/$SYSTEM_FOLDER/priv_validator_state.json.backup
+rm -rf $HOME/$SYSTEM_FOLDER/data/*
+mv $HOME/$SYSTEM_FOLDER/priv_validator_state.json.backup $HOME/$SYSTEM_FOLDER/data/priv_validator_state.json
+curl -L $SNAPSHOT | tar -I lz4 -xf - -C $HOME/$SYSTEM_FOLDER
 
 
 sudo systemctl daemon-reload
