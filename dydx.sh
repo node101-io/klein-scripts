c5eff3b79530e7ca5e39ebb7a1b57294d2d7149f
YakupAltay <yakup.altay@boun.edu.tr>
2023-06-24 02:37:43 +0300

    dydx.sh push

--- dydx.sh
+++ dydx.sh
@@ -1,57 +1,62 @@
-#!/bin/bash
+#! /bin/bash
 
-# Install necessary tools
+echo -e '\e[0m'                                                              
+echo -e '@@@  @@@   @@@@@@   @@@@@@@   @@@@@@@@    @@@   @@@@@@@@     @@@'
+echo -e '@@@@ @@@  @@@@@@@@  @@@@@@@@  @@@@@@@@   @@@@  @@@@@@@@@@   @@@@'
+echo -e '@@!@!@@@  @@!  @@@  @@!  @@@  @@!       @@@!!  @@!   @@@@  @@@!!'
+echo -e '!@!!@!@!  !@!  @!@  !@!  @!@  !@!         !@!  !@!  @!@!@    !@!'
+echo -e '@!@ !!@!  @!@  !@!  @!@  !@!  @!!!:!:     @!@  @!@ @! !@!    @!@'
+echo -e '!@!  !!!  !@!  !!!  !@!  !!!  !!!!!::     !@!  !@!!!  !!!    !@!'
+echo -e '!!:  !!!  !!:  !!!  !!:  !!!  !!:         !!:  !!:!   !!!    !!:'
+echo -e ':!:  !:!  :!:  !:!  :!:  !:!  :!:         :!:  :!:    !:!    :!:'
+echo -e ':::   ::   ::::::   :::::::   :::::::     :::   ::::::::     :::'
+echo -e '\e[0m'
+
+
+# Updates
 sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y && sudo apt install make clang pkg-config libssl-dev build-essential git jq ncdu bsdmainutils htop net-tools lsof -y < "/dev/null" && sudo apt-get update -y && sudo apt-get install wget liblz4-tool aria2 -y && sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu -y
 
-# Define variables
-EXECUTE=dydxprotocold
-CHAIN_ID=dydx-testnet-1
-MONIKER="Moniker"
-KEY_NAME="Key Name"
-VALIDATOR_ADDRESS="Validator Address"
-COSMOVISOR_REPO="provenance-io/cosmovisor"
-DYDXPROTOCOL_REPO="dydxprotocol/networks"
-DYDX_PLATFORM="<platform>"
+# Install Git
+sudo apt-get install -y git
 
-COSMOVISOR_VERSION=$(curl -sL https://api.github.com/repos/$COSMOVISOR_REPO/releases/latest | jq -r ".tag_name")
-BINARY_VERSION=$(curl -sL https://api.github.com/repos/$DYDXPROTOCOL_REPO/releases/latest | jq -r ".tag_name")
+# Install Go
+wget https://golang.org/dl/go1.20.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
+rm go1.20.5.linux-amd64.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+go version
 
-# Download and install Cosmovisor
-mkdir -p $HOME/cosmovisor
-wget https://github.com/$COSMOVISOR_REPO/releases/download/$COSMOVISOR_VERSION/cosmovisor_${COSMOVISOR_VERSION}_linux_amd64.tar.gz -P $HOME/cosmovisor
-tar -xvf $HOME/cosmovisor/cosmovisor_${COSMOVISOR_VERSION}_linux_amd64.tar.gz -C $HOME/cosmovisor
-sudo cp $HOME/cosmovisor/cosmovisor /usr/local/bin
+# Install cosmovisor
+echo "Installing Cosmovisor..."
+go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@latest
 
-# Clone the dydxprotocol networks repository and get the protocol binary
-git clone git@github.com:$DYDXPROTOCOL_REPO.git
+# Initialize node and setup validator
+export BINARY_VERSION="v0.0.1-rc1"
+export DYDX_PLATFORM="linux-amd64"
+export DYDX_MONIKER="node101"
+export DYDX_KEY_NAME="wallet101"
+
+git clone git@github.com:dydxprotocol/networks.git
 cd networks
 git checkout main
 git pull origin main
-ls $CHAIN_ID/binaries/$BINARY_VERSION
-tar -xvzf $CHAIN_ID/binaries/$BINARY_VERSION/$EXECUTE-$BINARY_VERSION-${DYDX_PLATFORM}.tar.gz
+tar -xvzf dydx-testnet-1/binaries/$BINARY_VERSION/dydxprotocold-$BINARY_VERSION-${DYDX_PLATFORM}.tar.gz
 mkdir -p "${HOME}/local/bin"
 export PATH="${HOME}/local/bin:$PATH"
-cp build/$EXECUTE-$BINARY_VERSION-${DYDX_PLATFORM} "${HOME}/local/bin/dydxprotocold"
-rm -R build 
-
-# Verify the binary version
-$EXECUTE version --long
-
-# Save the chain ID in dydxprotocold config
-$EXECUTE config chain-id $CHAIN_ID
-
-# Initialize the node
+cp build/dydxprotocold-$BINARY_VERSION-${DYDX_PLATFORM} "${HOME}/local/bin/dydxprotocold"
+rm -R build
+dydxprotocold version --long
+dydxprotocold config chain-id dydx-testnet-1
 rm -rf $HOME/.dydxprotocol
-$EXECUTE init --chain-id=$CHAIN_ID $MONIKER
+dydxprotocold init --chain-id=dydx-testnet-1 $DYDX_MONIKER
+dydxprotocold keys add $DYDX_KEY_NAME --keyring-backend test
+MY_VALIDATOR_ADDRESS=$(dydxprotocold keys show $DYDX_KEY_NAME -a --keyring-backend test)
+dydxprotocold add-genesis-account $MY_VALIDATOR_ADDRESS 100000000000dv4tnt
+dydxprotocold gentx $DYDX_KEY_NAME 50000000000dv4tnt --chain-id dydx-testnet-1 --moniker $DYDX_MONIKER --keyring-backend test
 
-# Add genesis account and generate gentx
-$EXECUTE add-genesis-account $VALIDATOR_ADDRESS 100000000000dv4tnt
-$EXECUTE gentx $KEY_NAME 50000000000dv4tnt --chain-id $CHAIN_ID --moniker $MONIKER --keyring-backend test
-
-# Submit gentx
-git clone git@github.com:$DYDXPROTOCOL_REPO.git
-git checkout -b $MONIKER/gentx
-cp ~/.dydxprotocol/config/gentx/gentx-xxxxxxxxxxxx.json networks/$CHAIN_ID/gentx/gentx-$MONIKER.json
-git add $CHAIN_ID/gentx/*
-git commit -m "$MONIKER gentx"
-git push origin $MONIKER/gentx
+Starting the Node
+git checkout main
+git pull origin main
+cp dydx-testnet-1/genesis.json $HOME/.dydxprotocol/config/genesis.json
+dydxprotocold start --p2p.seeds="25dd504d86d82673b9cf94fe78c00714f236c9f8@3.128.51.185:26656"
