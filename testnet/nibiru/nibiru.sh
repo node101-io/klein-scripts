367c7a110df90efcda3f7847108d33d2e476e957
YakupAltay <yakup.altay@boun.edu.tr>
2023-07-12 15:49:17 +0300

    nibiru.sh dynamic version and cosmovisor

--- testnet/nibiru/nibiru.sh
+++ testnet/nibiru/nibiru.sh
@@ -18,12 +18,13 @@ CHAIN_ID=nibiru-itn-1
 REPO=https://github.com/NibiruChain/nibiru.git
 PROJECT_FOLDER=nibiru
 SYSTEM_FOLDER=.nibid
-VERSION=v0.19.2
+RPC_URL=https://nibiru-testnet.rpc.kjnodes.com
+VERSION=$(curl -s -L "${RPC_URL}/abci_info?" | jq -r '.result.response.version')
 GENESIS_FILE=https://snapshots.kjnodes.com/nibiru-testnet/genesis.json
 ADDRBOOK=https://snapshots.kjnodes.com/nibiru-testnet/addrbook.json
 PORT=26
 DENOM=unibi
-GO_VERSION=$(curl -L https://golang.org/VERSION?m=text | sed 's/^go//') 
+GO_VERSION=$(curl -L https://golang.org/VERSION?m=text | sed 's/^go//')
 PEERS="e2ad22b7cefbddd747c29d90882561e566ff2d3e@65.109.50.106:26656,74405e27923c1efe97fc678aa9f0357537a9b311@161.97.64.38:26656,b3a2a298c6a84c503253d120e3eee0c54cea90fd@137.184.193.235:20356,b9f203a7d45a2a2766ff144ea9cc680987886772@85.239.242.186:26656,bd4e84bd7b14201661c958c6cb6a1de2a27078ed@95.217.156.62:26656,0faa013496da308cf091099bb736f512f17ab380@185.144.99.55:26656,a03eaa525bd984d713fd9b000a89163dc7516a83@185.207.250.222:26656,5160aa16c18a37d25c3e667c80de83279715f2aa@195.2.75.252:26656,d5519e378247dfb61dfe90652d1fe3e2b3005a5b@65.109.68.190:13956,ce2dcfe5794bed1d4b32b8a43b32afc5d5e435f2@135.181.114.98:46656,f98a8229e5dc6da6d5e49fd4e115472df3d1773c@95.9.36.100:26656,b253cc6155ec59ea623f3f453d2f5a4b9c6d08fc@212.15.59.91:39656"
 SEEDS="3f472746f46493309650e5a033076689996c8881@nibiru-testnet.rpc.kjnodes.com:13959"
 
@@ -33,6 +34,7 @@ echo "export EXECUTE=${EXECUTE}" >> $HOME/.bash_profile
 echo "export CHAIN_ID=${CHAIN_ID}" >> $HOME/.bash_profile
 echo "export SYSTEM_FOLDER=${SYSTEM_FOLDER}" >> $HOME/.bash_profile
 echo "export PROJECT_FOLDER=${PROJECT_FOLDER}" >> $HOME/.bash_profile
+echo "export RPC_URL=${RPC_URL}" >> $HOME/.bash_profile
 echo "export VERSION=${VERSION}" >> $HOME/.bash_profile
 echo "export REPO=${REPO}" >> $HOME/.bash_profile
 echo "export GENESIS_FILE=${GENESIS_FILE}" >> $HOME/.bash_profile
@@ -142,29 +144,16 @@ sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $
 # Set minimum gas price
 sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.025$DENOM\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
 
-
-# Creating your systemd service
-sudo tee <<EOF >/dev/null /etc/systemd/system/$EXECUTE.service
-[Unit]
-Description=$EXECUTE
-After=network-online.target
-
-[Service]
-User=$USER
-ExecStart=$(which $EXECUTE) start --home $HOME/$SYSTEM_FOLDER
-Restart=on-failure
-RestartSec=10
-LimitNOFILE=65535
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
 sleep 3 
 
 #fast sync with snapshot
 SNAPSHOT=https://snapshots.kjnodes.com/nibiru-testnet/snapshot_latest.tar.lz4
-curl -L $SNAPSHOT | tar -Ilz4 -xf - -C $HOME/$SYSTEM_FOLDER
+cp $HOME/$SYSTEM_FOLDER/data/priv_validator_state.json $HOME/$SYSTEM_FOLDER/priv_validator_state.json.backup
+rm -rf $HOME/$SYSTEM_FOLDER/data/*
+mv $HOME/$SYSTEM_FOLDER/priv_validator_state.json.backup $HOME/$SYSTEM_FOLDER/data/priv_validator_state.json
+curl -L $SNAPSHOT | tar -I lz4 -xf - -C $HOME/$SYSTEM_FOLDER
+
+# Upgrade info
 [[ -f $HOME/$SYSTEM_FOLDER/data/upgrade-info.json ]] && cp $HOME/$SYSTEM_FOLDER/data/upgrade-info.json $HOME/$SYSTEM_FOLDER/cosmovisor/genesis/upgrade-info.json
 
 sudo systemctl daemon-reload
