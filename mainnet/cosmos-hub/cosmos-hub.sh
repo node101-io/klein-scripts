882a891337d26c7f3bb6228e5cbe86589291e002
YakupAltay <yakup.altay@boun.edu.tr>
2023-09-26 17:37:45 +0300

    mainnet: cosmos-hub.sh & osmosis.sh added

--- mainnet/cosmos-hub/cosmos-hub.sh
+++ mainnet/cosmos-hub/cosmos-hub.sh
@@ -20,19 +20,22 @@ echo "5 installation_progress"
 # Variables
 PROJECT=cosmos
 EXECUTE=gaiad
-CHAIN_ID=cosmoshub-4
-SYSTEM_FOLDER=.gaia
-PROJECT_FOLDER=gaia
-RPC_URL=https://cosmoshub.rpc.kjnodes.com
+RPC_URL=https://cosmos-rpc.polkachu.com
+CHAIN_ID=$(curl -s -L "${RPC_URL}/status?" | jq -r '.result.node_info.network')
 VERSION=$(curl -s -L "${RPC_URL}/abci_info?" | jq -r '.result.response.version')
 REPO=https://github.com/cosmos/gaia.git
+PROJECT_FOLDER=gaia
+SYSTEM_FOLDER=.gaia
 GENESIS_FILE=https://snapshots.polkachu.com/genesis/cosmos/genesis.json
 ADDRBOOK=https://snapshots.polkachu.com/addrbook/cosmos/addrbook.json
-PORT=26
 DENOM=uatom
-GO_VERSION="1.20"
-PEERS="d6318b3bd51a5e2b8ed08f2e520d50289ed32bf1@52.79.43.100:26656,b0e746acb6fbed7a0311fe21cfb2ee94581ca3bc@51.79.21.187:26656,1da54d20c7339713f1d6d28dd2117087dd33d0ca@cosmos-seed.icycro.org:26656,fe21dd474640247888fc7c4dce82da8da08a8bfd@peer-cosmos-hub-01.stakeflow.io:26656,01c0d24922dcdf6f8816ec814a5c3436c5d5fbc5@65.108.195.29:36656,28d36c3d45f0208528de3c38f2934ae241bd23e7@peer-cosmoshub.mms.team:26656"
+GO_VERSION=$(curl -L https://golang.org/VERSION?m=text | grep '^go' | sed 's/^go//')
+PEERS="ca5011c44fd74d95e7fca487c69e301df195750c@65.108.122.246:26726,2a3b000f3ecfd3ec5f030212727090cdf84a7ec6@141.95.157.139:14956,c07f1646f478565722d7484ae164f433069a48d2@54.36.231.142:26656,1cce99042f884d669e7287e3e362bff8e385c63e@46.4.79.183:26726,6fe1585040f121411cf57571bf8881d9975ef2b8@204.16.247.254:26656,3dcea0d12f15c300cb0e882bc8f079ebfe3270eb@139.59.8.48:26090,b9a6c96710f73a71486a0b04e09c4b74f66b2f50@3.142.220.32:26656,e829d4764a5cecc44b3414777853b34407b36601@198.244.202.140:26656,05d870293f89e0698a8bd198e31f6ca17baa3a17@52.78.207.43:26656,6ea2ef7d3dd5d6967708a0b31eed85ba090a90a1@65.108.121.190:12010,160524d2ad8e475dadfa64db9c341767064b5e92@148.251.183.254:12010,4c46d32cbc4777c59a91a53fdadf8a3fa362036e@116.202.10.68:26656,1da54d20c7339713f1d6d28dd2117087dd33d0ca@5.9.59.145:26656,f52b6ca356060842431aa96392af4e9fdeaec436@67.209.53.70:26656,a09ed43e09f773e39855dc5d8b6a220eff4cb947@204.16.241.207:26656,155ee9292bf5212c9577841018131428939e0a85@128.199.128.15:26090,4863934de1bf496ca5702e0450706f785091dda1@77.109.139.19:26656,c1e437f73b8889b78ea34981e7c349157ad80284@107.135.15.66:26656,9edd51012df3a09395a48eb68a84723d6308e08c@35.212.116.100:26656,1a2ec6b643e4f6fda476dc94f579c2035e1de33f@178.128.238.183:26090,4cb59a50805f0646111412cf5d189e805e3e0c3d@89.149.218.64:26656,70ad5b4f44a993a27f5dc21e20c784d5747ea136@65.108.234.179:31440,07da4ffc5f6ed24960503ad046811f879e7f1823@44.214.37.79:26656,0b998008bb995cc868ed7c6d569b8aaee51a1350@23.111.186.178:26656,b79e1d3a621bdafd3a8d9a49dff8f4737d0bedc9@52.203.105.100:26656,6c82dc76b32fd49f6d538c04c84c7daad1061bb4@51.159.162.222:26656,ce526dc00568900d54dedf2fd2680ddfc7a58c59@138.201.124.215:26656,59be1d1bf8a50e8b1fe4a17c4ebe523d8d0fb2f3@51.91.152.102:16181,d9bfa29e0cf9c4ce0cc9c26d98e5d97228f93b0b@65.108.233.103:13456,5b4529df65f9c1006d51472a827f1deb23825ba2@91.194.30.204:14656"
 SEEDS="ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@seeds.polkachu.com:14956"
+
+if [[ $VERSION != v* ]]; then
+  VERSION="v$VERSION"
+fi
 
 sleep 2
 
@@ -44,7 +47,6 @@ echo "export VERSION=${VERSION}" >> $HOME/.bash_profile
 echo "export REPO=${REPO}" >> $HOME/.bash_profile
 echo "export GENESIS_FILE=${GENESIS_FILE}" >> $HOME/.bash_profile
 echo "export ADDRBOOK=${ADDRBOOK}" >> $HOME/.bash_profile
-echo "export PORT=${PORT}" >> $HOME/.bash_profile
 echo "export DENOM=${DENOM}" >> $HOME/.bash_profile
 echo "export GO_VERSION=${GO_VERSION}" >> $HOME/.bash_profile
 echo "export PEERS=${PEERS}" >> $HOME/.bash_profile
@@ -83,15 +85,17 @@ make build
 make install
 sleep 1
 
+# Prepare binaries for Cosmovisor
+mkdir -p $HOME/${SYSTEM_FOLDER}/cosmovisor/genesis/bin
+mv build/${EXECUTE} $HOME/${SYSTEM_FOLDER}/cosmovisor/genesis/bin/
+rm -rf build
+
+# Create application symlinks
+sudo ln -s $HOME/${SYSTEM_FOLDER}/cosmovisor/genesis $HOME/${SYSTEM_FOLDER}/cosmovisor/current -f
+sudo ln -s $HOME/${SYSTEM_FOLDER}/cosmovisor/current/bin/${EXECUTE} /usr/local/bin/${EXECUTE} -f
+
 # Download and install Cosmovisor
 go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
-
-# Create Cosmovisor Folders
-mkdir -p ~/${SYSTEM_FOLDER}/cosmovisor/genesis/bin
-mkdir -p ~/${SYSTEM_FOLDER}/cosmovisor/upgrades
-
-# Load Node Binary into Cosmovisor Folder
-cp ~/go/bin/$EXECUTE ~/${SYSTEM_FOLDER}/cosmovisor/genesis/bin
 
 # Create service
 sudo tee /etc/systemd/system/${EXECUTE}.service > /dev/null << EOF
@@ -116,7 +120,7 @@ EOF
 
 $EXECUTE config chain-id $CHAIN_ID
 $EXECUTE config keyring-backend test
-$EXECUTE config node tcp://localhost:${PORT}657
+$EXECUTE config node tcp://localhost:26657
 $EXECUTE init $MONIKER --chain-id $CHAIN_ID
 
 # Set peers and seeds
@@ -163,5 +167,5 @@ echo "export NODE_PROPERLY_INSTALLED=true" >> $HOME/.bash_profile
 
 echo '=============== SETUP IS FINISHED ==================='
 echo -e "CHECK OUT YOUR LOGS : \e[1m\e[32mjournalctl -fu ${EXECUTE} -o cat\e[0m"
-echo -e "CHECK SYNC: \e[1m\e[32mcurl -s localhost:${PORT}657/status | jq .result.sync_info\e[0m"
+echo -e "CHECK SYNC: \e[1m\e[32mcurl -s localhost:26657/status | jq .result.sync_info\e[0m"
 source $HOME/.bash_profile
