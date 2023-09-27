93204755a1a4debe81465cd0f8e9626db5bb009f
YakupAltay <yakup.altay@boun.edu.tr>
2023-09-27 17:19:58 +0300

    quasar.sh: added with the new template

---
+++ testnet/quasar/quasar.sh
@@ -0,0 +1,174 @@
+#! /bin/bash
+
+echo -e '\e[0m'                                                              
+echo -e '@@@  @@@   @@@@@@   @@@@@@@   @@@@@@@@    @@@   @@@@@@@@     @@@'
+echo -e '@@@@ @@@  @@@@@@@@  @@@@@@@@  @@@@@@@@   @@@@  @@@@@@@@@@   @@@@'
+echo -e '@@!@!@@@  @@!  @@@  @@!  @@@  @@!       @@@!!  @@!   @@@@  @@@!!'
+echo -e '!@!!@!@!  !@!  @!@  !@!  @!@  !@!         !@!  !@!  @!@!@    !@!'
+echo -e '@!@ !!@!  @!@  !@!  @!@  !@!  @!!!:!:     @!@  @!@ @! !@!    @!@'
+echo -e '!@!  !!!  !@!  !!!  !@!  !!!  !!!!!::     !@!  !@!!!  !!!    !@!'
+echo -e '!!:  !!!  !!:  !!!  !!:  !!!  !!:         !!:  !!:!   !!!    !!:'
+echo -e ':!:  !:!  :!:  !:!  :!:  !:!  :!:         :!:  :!:    !:!    :!:'
+echo -e ':::   ::   ::::::   :::::::   :::::::     :::   ::::::::     :::'
+echo -e '\e[0m'
+
+# Updates
+sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y && sudo apt install make clang pkg-config libssl-dev build-essential git jq ncdu bsdmainutils htop net-tools lsof -y < "/dev/null" && sudo apt-get update -y && sudo apt-get install wget liblz4-tool aria2 -y && sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu -y
+
+echo "5 installation_progress"
+
+# Variables
+PROJECT=quasar
+EXECUTE=quasarnoded
+RPC_URL=https://quasar-testnet-rpc.polkachu.com
+CHAIN_ID=$(curl -s -L "${RPC_URL}/status?" | jq -r '.result.node_info.network')
+VERSION=$(curl -s -L "${RPC_URL}/abci_info?" | jq -r '.result.response.version')
+REPO=https://github.com/quasar-finance/quasar.git
+PROJECT_FOLDER=$(echo $REPO | awk -F/ '{print $NF}' | sed 's/.git$//')
+SYSTEM_FOLDER=.quasarnode
+GENESIS_FILE=https://snapshots.kjnodes.com/quasar-testnet/genesis.json
+ADDRBOOK=https://snapshots.kjnodes.com/quasar-testnet/addrbook.json
+DENOM=uqsr
+GO_VERSION=$(curl -L https://golang.org/VERSION?m=text | grep '^go' | sed 's/^go//')
+PEERS="6c07be1d77a89bc49a8ef54cb57bd1a6f957afd7@116.202.227.117:14856,66792d442aeaa320d895e10e3e2a02a3c0b6be30@3.76.85.22:27766,56e6e150fc9006e693d6261d0a473341b9b07111@141.94.138.48:26661,d828ac8d07de45c610a8cd02511dba23f1d55719@34.175.134.141:26656,265bc06fe615ee1851a11ca41c861bd0e501da56@34.175.185.244:26656,e220aee1ffc5de743772be6e9735248f24520aa3@5.161.211.145:26656,7f99c242002c80b6d1f8e3bda1f2d6c66a215e32@38.109.200.33:26656,d5519e378247dfb61dfe90652d1fe3e2b3005a5b@65.109.68.190:14856,2b215d1e09d3eab52c7abf1bf2f838e457b355ec@63.229.234.75:26656,658d240b1c291879d49eb07631f7fbf1af55ca22@208.88.251.50:26656,20b4f9207cdc9d0310399f848f057621f7251846@222.106.187.13:20600,f12f8926889753ba8bdaf508aa26b3b0f60693ad@185.111.159.185:18256,56e0289842252644780e6e49407824186dd31f6f@65.109.23.114:18256,52c2ec16b02f27926b15ce20369ecc89cc32143e@65.108.226.183:18256,375a596b93235ad511d3eb85d90302492e43210a@34.175.190.31:26656,17a5d07b9950e1a3ed1f9e7a876132b783d23876@213.133.100.172:26878"
+SEEDS="ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@testnet-seeds.polkachu.com:18256"
+
+if [[ $VERSION != v* ]]; then
+  VERSION="v$VERSION"
+fi
+
+sleep 2
+
+echo "export EXECUTE=${EXECUTE}" >> $HOME/.bash_profile
+echo "export CHAIN_ID=${CHAIN_ID}" >> $HOME/.bash_profile
+echo "export SYSTEM_FOLDER=${SYSTEM_FOLDER}" >> $HOME/.bash_profile
+echo "export PROJECT_FOLDER=${PROJECT_FOLDER}" >> $HOME/.bash_profile
+echo "export VERSION=${VERSION}" >> $HOME/.bash_profile
+echo "export REPO=${REPO}" >> $HOME/.bash_profile
+echo "export GENESIS_FILE=${GENESIS_FILE}" >> $HOME/.bash_profile
+echo "export ADDRBOOK=${ADDRBOOK}" >> $HOME/.bash_profile
+echo "export PORT=${PORT}" >> $HOME/.bash_profile
+echo "export DENOM=${DENOM}" >> $HOME/.bash_profile
+echo "export GO_VERSION=${GO_VERSION}" >> $HOME/.bash_profile
+echo "export PEERS=${PEERS}" >> $HOME/.bash_profile
+echo "export SEEDS=${SEEDS}" >> $HOME/.bash_profile
+
+source $HOME/.bash_profile
+
+sleep 1
+if [ ! $MONIKER ]; then
+	read -p "ENTER MONIKER NAME: " MONIKER
+	echo 'export MONIKER='$MONIKER >> $HOME/.bash_profile
+fi
+
+echo "30 installation_progress"
+
+# Go installation
+cd $HOME
+wget "https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$GO_VERSION.linux-amd64.tar.gz"
+rm "go$GO_VERSION.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+go version
+
+echo "60 installation_progress"
+
+sleep 1
+
+cd $HOME
+rm -rf $PROJECT_FOLDER
+git clone $REPO
+cd $PROJECT_FOLDER
+git checkout $VERSION
+make build
+make install
+sleep 1
+
+# Prepare binaries for Cosmovisor
+mkdir -p $HOME/${SYSTEM_FOLDER}/cosmovisor/genesis/bin
+mv build/${EXECUTE} $HOME/${SYSTEM_FOLDER}/cosmovisor/genesis/bin/
+rm -rf build
+
+# Create application symlinks
+sudo ln -s $HOME/${SYSTEM_FOLDER}/cosmovisor/genesis $HOME/${SYSTEM_FOLDER}/cosmovisor/current -f
+sudo ln -s $HOME/${SYSTEM_FOLDER}/cosmovisor/current/bin/${EXECUTE} /usr/local/bin/${EXECUTE} -f
+
+# Download and install Cosmovisor
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
+
+# Create service
+sudo tee /etc/systemd/system/${EXECUTE}.service > /dev/null << EOF
+[Unit]
+Description=${EXECUTE}
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/${SYSTEM_FOLDER}"
+Environment="DAEMON_NAME=${EXECUTE}"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/${SYSTEM_FOLDER}/cosmovisor/current/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+$EXECUTE config chain-id $CHAIN_ID
+$EXECUTE config keyring-backend test
+$EXECUTE config node tcp://localhost:26657
+$EXECUTE init $MONIKER --chain-id $CHAIN_ID
+
+# Set peers and seeds
+sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|" $HOME/$SYSTEM_FOLDER/config/config.toml
+sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/$SYSTEM_FOLDER/config/config.toml
+
+# Download genesis and addrbook
+curl -Ls $GENESIS_FILE > $HOME/$SYSTEM_FOLDER/config/genesis.json
+curl -Ls $ADDRBOOK > $HOME/$SYSTEM_FOLDER/config/addrbook.json
+
+echo "75 installation_progress"
+
+# Set Config Pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="10"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+
+# Set minimum gas price
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0025$DENOM\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+
+sleep 3 
+
+#fast sync with snapshot
+wget -q -O - https://polkachu.com/testnets/${PROJECT}/snapshots > webpage.html
+SNAPSHOT=$(grep -o "https://snapshots.polkachu.com/testnet-snapshots/${PROJECT}/${PROJECT}_[0-9]*.tar.lz4" webpage.html | head -n 1)
+cp $HOME/$SYSTEM_FOLDER/data/priv_validator_state.json $HOME/$SYSTEM_FOLDER/priv_validator_state.json.backup
+rm -rf $HOME/$SYSTEM_FOLDER/data/*
+mv $HOME/$SYSTEM_FOLDER/priv_validator_state.json.backup $HOME/$SYSTEM_FOLDER/data/priv_validator_state.json
+curl -L $SNAPSHOT | tar -I lz4 -xf - -C $HOME/$SYSTEM_FOLDER
+
+# Upgrade info
+[[ -f $HOME/$SYSTEM_FOLDER/data/upgrade-info.json ]] && cp $HOME/$SYSTEM_FOLDER/data/upgrade-info.json $HOME/$SYSTEM_FOLDER/cosmovisor/genesis/upgrade-info.json
+
+sudo systemctl daemon-reload
+sudo systemctl enable $EXECUTE
+sudo systemctl restart $EXECUTE
+
+echo "export NODE_PROPERLY_INSTALLED=true" >> $HOME/.bash_profile
+
+echo '=============== SETUP IS FINISHED ==================='
+echo -e "CHECK OUT YOUR LOGS : \e[1m\e[32mjournalctl -fu ${EXECUTE} -o cat\e[0m"
+echo -e "CHECK SYNC: \e[1m\e[32mcurl -s localhost:26657/status | jq .result.sync_info\e[0m"
+source $HOME/.bash_profile
+
+rm -- "$0"
