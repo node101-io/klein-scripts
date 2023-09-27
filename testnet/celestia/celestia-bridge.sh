60fe35cfd7ccc70f620d2235a6a80fa43052715b
YakupAltay <yakup.altay@boun.edu.tr>
2023-09-28 00:51:41 +0300

    celestia-bridge.sh: bridge node commands added

--- testnet/celestia/celestia-bridge.sh
+++ testnet/celestia/celestia-bridge.sh
@@ -13,103 +13,74 @@ echo -e ':::   ::   ::::::   :::::::   :::::::     :::   ::::::::     :::'
 echo -e '\e[0m'
 
 # Variables
+BRIDGE_REPO=https://github.com/celestiaorg/celestia-node.git
+BRIDGE_PROJECT_FOLDER=celestia-node
+BRIDGE_VERSION=v0.11.0-rc12
 
-
-GO_VERSION="1.19.3"
-REPO="https://github.com/celestiaorg/celestia-app.git"
-VERSION=v0.12.1
-NODEREPO="https://github.com/celestiaorg/celestia-node.git"
-NODEVER=v0.8.0
-EXECUTE="celestia-bridge"
-PORT=26
-SYSTEM_FOLDER=.celestia-app
-PROJECT_FOLDER=celestia-app
-DENOM="utia"
-sleep 2
-
-echo "export GO_VERSION=${GO_VERSION}" >> $HOME/.bash_profile
-echo "export REPO=${REPO}" >> $HOME/.bash_profile
-echo "export VERSION=${VERSION}" >> $HOME/.bash_profile
-echo "export NODEREPO=${NODEREPO}" >> $HOME/.bash_profile
-echo "export NODEVER=${NODEVER}" >> $HOME/.bash_profile
-echo "export EXECUTE=${EXECUTE}" >> $HOME/.bash_profile
-echo "export PORT=${PORT}" >> $HOME/.bash_profile
-echo "export SYSTEM_FOLDER=${SYSTEM_FOLDER}" >> $HOME/.bash_profile
-echo "export PROJECT_FOLDER=${PROJECT_FOLDER}" >> $HOME/.bash_profile
-echo "export DENOM=${DENOM}" >> $HOME/.bash_profile
-
-source $HOME/.bash_profile
-
-sleep 1
-
-# Updates
-sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y && sudo apt install make clang pkg-config libssl-dev build-essential git jq ncdu bsdmainutils htop net-tools lsof -y < "/dev/null" && sudo apt-get update -y && sudo apt-get install wget liblz4-tool aria2 -y && sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu -y
-
-echo "15 installation_progress"
-
-cd $HOME
-wget "https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$GO_VERSION.linux-amd64.tar.gz"
-rm "go$GO_VERSION.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
-source $HOME/.bash_profile
-go version
-
-echo "45 installation_progress"
-
-sleep 1
-
-cd $HOME
-rm -rf $PROJECT_FOLDER
-git clone $REPO
-cd $PROJECT_FOLDER
-git checkout tags/$VERSION -b $VERSION
-make install
-
-sleep 1
-
-#Clone celestia node
-
-cd $HOME 
-rm -rf celestia-node
-git clone $NODEREPO
-cd celestia-node 
-git checkout tags/$NODEVER
+# Download and build binaries
+rm -rf $BRIDGE_PROJECT_FOLDER
+git clone $BRIDGE_REPO
+cd $BRIDGE_PROJECT_FOLDER
+git checkout $BRIDGE_VERSION
 make build
-make install
+sudo mv build/celestia /usr/local/bin
 make cel-key
+sudo mv cel-key /usr/local/bin
 
+# Add wallet
+cel-key add bridge-wallet --node.type bridge --p2p.network mocha
 
-./cel-key add my_celes_key --node.type bridge --p2p.network blockspacerace
+# Init bridge node
+celestia bridge init \
+  --keyring.accname bridge-wallet \
+  --core.ip http://localhost \
+  --core.rpc.port 12057 \
+  --core.grpc.port 12090 \
+  --p2p.network mocha \
+  --rpc.port 12058 \
+  --gateway.port 12059
 
-cd ~/celestia-node
-celestia bridge init --keyring.accname my_celes_key --core.ip https://rpc-blockspacerace.pops.one/ --p2p.network blockspacerace
-
-echo "75 installation_progress"
-
-# Creating your systemd service
-sudo tee <<EOF >/dev/null /etc/systemd/system/$EXECUTE.service
+# Create service
+sudo tee /etc/systemd/system/celestia-bridge.service > /dev/null << EOF
 [Unit]
-Description=$EXECUTE Bridge Node
+Description=Celestia Bridge Node service
 After=network-online.target
+
 [Service]
-User=root
-ExecStart=/usr/local/bin/celestia bridge start --core.ip https://rpc-blockspacerace.pops.one/ --core.rpc.port 26657 --core.grpc.port 9090 --keyring.accname my_celes_key --metrics.tls=false --metrics --metrics.endpoint otel.celestia.tools:4318 --gateway --gateway.addr localhost --gateway.port 26659 --p2p.network blockspacerace
+User=$USER
+ExecStart=$(which celestia) bridge start \\
+--keyring.accname bridge-wallet \\
+--core.ip http://localhost \\
+--core.rpc.port 12057 \\
+--core.grpc.port 12090 \\
+--p2p.network mocha \\
+--rpc.port 12058 \\
+--gateway.port 12059 \\
+--metrics.tls=false \\
+--metrics \\
+--metrics.endpoint otel.celestia.tools:4318 
 Restart=on-failure
-RestartSec=3
-LimitNOFILE=4096
+RestartSec=10
+LimitNOFILE=65535
+
 [Install]
 WantedBy=multi-user.target
 EOF
+sudo systemctl daemon-reload
+sudo systemctl enable celestia-bridge
 
+# start bridge node
+systemctl restart celestia-bridge
 
-systemctl daemon-reload
-sudo systemctl enable $EXECUTE
-sudo systemctl restart $EXECUTE
+# Check logs
+journalctl -fu celestia-bridge -o cat
 
-echo "export NODE_PROPERLY_INSTALLED=true" >> $HOME/.bash_profile
+# Bridge node ID
+AUTH_TOKEN=$(celestia bridge auth admin --p2p.network mocha)
+curl -s -X POST -H "Authorization: Bearer $AUTH_TOKEN" -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":0,"method":"p2p.Info","params":[]}' http://localhost:12058 | jq -r .result.ID
 
-echo '=============== SETUP IS FINISHED ==================='
-echo -e "CHECK OUT YOUR LOGS : \e[1m\e[32mjournalctl -fu $EXECUTE -o cat\e[0m"
+# Bridge node key
+cel-key show bridge-wallet --node.type bridge --p2p.network mocha -a | tail -1
 
+# Bridge node balance
+celestia-appd q bank balances $(cel-key show bridge-wallet --node.type bridge --p2p.network mocha -a | tail -1)
