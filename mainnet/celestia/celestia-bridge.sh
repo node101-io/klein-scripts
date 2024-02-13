2951b4b14a4b88f661c3406eaf3a6463e9caa53c
YakupAltay <yakup.altay@boun.edu.tr>
2024-02-13 17:33:39 +0300

    Update Celestia Bridge script and dependencies

--- mainnet/celestia/celestia-bridge.sh
+++ mainnet/celestia/celestia-bridge.sh
@@ -12,17 +12,31 @@ echo -e ':!:  !:!  :!:  !:!  :!:  !:!  :!:         :!:  :!:    !:!    :!:'
 echo -e ':::   ::   ::::::   :::::::   :::::::     :::   ::::::::     :::'
 echo -e '\e[0m'
 
+sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y && sudo apt install make clang pkg-config libssl-dev build-essential git jq ncdu bsdmainutils htop net-tools lsof -y < "/dev/null" && sudo apt-get update -y && sudo apt-get install wget liblz4-tool aria2 -y && sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu -y
+
+
 # Variables
 BRIDGE_REPO=https://github.com/celestiaorg/celestia-node.git
 BRIDGE_PROJECT_FOLDER=celestia-node
-BRIDGE_VERSION=v0.12.1
+BRIDGE_VERSION=v0.12.4
+GO_VERSION="1.21.5"
 
 # Export variables
 echo "export BRIDGE_REPO=$BRIDGE_REPO" >> ~/.bash_profile
 echo "export BRIDGE_PROJECT_FOLDER=$BRIDGE_PROJECT_FOLDER" >> ~/.bash_profile
 echo "export BRIDGE_VERSION=$BRIDGE_VERSION" >> ~/.bash_profile
+echo "export GO_VERSION=$GO_VERSION" >> ~/.bash_profile
 
 source $HOME/.bash_profile
+
+cd $HOME
+wget "https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$GO_VERSION.linux-amd64.tar.gz"
+rm "go$GO_VERSION.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+go version
 
 # Download and build binaries
 cd $HOME
@@ -44,11 +58,11 @@ cel-key add bridge-wallet --node.type bridge --p2p.network celestia
 celestia bridge init \
   --keyring.accname bridge-wallet \
   --core.ip http://localhost \
-  --core.rpc.port 26657 \
-  --core.grpc.port 9090 \
+  --core.rpc.port 36657 \
+  --core.grpc.port 1090 \
   --p2p.network celestia \
-  --rpc.port 26658 \
-  --gateway.port 26659
+  --rpc.port 36658 \
+  --gateway.port 36659
 
 # Create service
 sudo tee /etc/systemd/system/celestia-bridge.service > /dev/null << EOF
@@ -61,11 +75,11 @@ User=$USER
 ExecStart=$(which celestia) bridge start \\
 --keyring.accname bridge-wallet \\
 --core.ip http://localhost \\
---core.rpc.port 26657 \\
---core.grpc.port 9090 \\
+--core.rpc.port 36657 \\
+--core.grpc.port 1090 \\
 --p2p.network celestia \\
---rpc.port 26658 \\
---gateway.port 26659 \\
+--rpc.port 36658 \\
+--gateway.port 36659 \\
 --metrics.tls=true \\
 --metrics \\
 --metrics.endpoint otel.celestia.observer 
