628cc48e3e29d66a1b1833c41962b5cb2c383e77
YakupAltay <yakup.altay@boun.edu.tr>
2024-03-02 13:35:27 +0300

    Add Regen Network installation script

---
+++ mainnet/regen-network/regen-network.sh
@@ -0,0 +1,173 @@
+#! /bin/bash
+
+echo -e '\e[0m'
+echo -e '@@@  @@@   @@@@@@   @@@@@@@   @@@@@@@@    @@@   @@@@@@@@     @@@'
+echo -e '@@@@ @@@  @@@@@@@@  @@@@@@@@  @@@@@@@@   @@@@  @@@@@@@@@@   @@@@'
+echo -e '@@!@!@@@  @@!  @@@  @@!  @@@  @@!       @@@!!  @@!   @@@@  @@@!!'
+echo -e '!@!!@!@!  !@!  @!@  !@!  @!@  !@!         !@!  !@!  @!@!@    !@!'
+echo -e '@!@ !!@!  @!@  !@!  @!@  !@!  @!!!:!:     @!@  @!@ @! !@!    @!@'
+echo -e '!@!  !!!  !@!  !!!  !@!  !!!  !!!!!::     !@!  !@!!!  !!!    !@!'
+echo -e '!!:  !!!  !!:  !!!  !!:  !!!  !!:         !!:  !!:!   !!!    !!:'
+echo -e ':!:  !:!  :!:  !:!  :!:  !:!  :!:         :!:  :!:    !:!    :!:'
+echo -e ':::   ::   ::::::   :::::::   :::::::     :::   ::::::::     :::'
+echo -e '\e[0m'
+
+# Updates
+sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y && sudo apt install make clang pkg-config libssl-dev build-essential git jq ncdu bsdmainutils htop net-tools lsof -y < "/dev/null" && sudo apt-get update -y && sudo apt-get install wget liblz4-tool aria2 -y && sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu -y
+
+echo "5 installation_progress"
+
+# Variables
+PROJECT=regen
+EXECUTE=regen
+SYSTEM_FOLDER=.regen
+PROJECT_FOLDER=regen-ledger
+RPC_URL=https://regen-rpc.publicnode.com
+CHAIN_ID=$(curl -s -L "${RPC_URL}/status?" | jq -r '.result.node_info.network')
+VERSION=$(curl -s -L "${RPC_URL}/abci_info?" | jq -r '.result.response.version')
+REPO=https://github.com/regen-network/regen-ledger.git
+GENESIS_FILE=https://raw.githubusercontent.com/regen-network/mainnet/main/regen-1/genesis.json
+ADDRBOOK=http://snapshots.autostake.com/lyIs25DaSWMSm8evWKHGQrb/regen-1/addrbook.json
+PORT=26
+DENOM=uregen
+GO_VERSION=$(curl -L https://golang.org/VERSION?m=text | grep '^go' | sed 's/^go//')
+PEERS=$(curl -sS ${RPC_URL}/net_info | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):\(.node_info.listen_addr)"' | awk -F ':' '{print $1":"$(NF)}' | head -n 5 | paste -sd, -)
+SEEDS="ebc272824924ea1a27ea3183dd0b9ba713494f83@regen-mainnet-seed.autostake.com:27216"
+
+if [[ $VERSION != v* ]]; then
+  VERSION="v$VERSION"
+fi
+
+sleep 2
+
+echo "export EXECUTE=${EXECUTE}" >> $HOME/.bash_profile
+echo "export CHAIN_ID=${CHAIN_ID}" >> $HOME/.bash_profile
+echo "export SYSTEM_FOLDER=${SYSTEM_FOLDER}" >> $HOME/.bash_profile
+echo "export PROJECT_FOLDER=${PROJECT_FOLDER}" >> $HOME/.bash_profile
+echo "export VERSION=${VERSION}" >> $HOME/.bash_profile
+echo "export REPO=${REPO}" >> $HOME/.bash_profile
+echo "export GENESIS_FILE=${GENESIS_FILE}" >> $HOME/.bash_profile
+echo "export ADDRBOOK=${ADDRBOOK}" >> $HOME/.bash_profile
+echo "export PORT=${PORT}" >> $HOME/.bash_profile
+echo "export DENOM=${DENOM}" >> $HOME/.bash_profile
+echo "export GO_VERSION=${GO_VERSION}" >> $HOME/.bash_profile
+echo "export PEERS=${PEERS}" >> $HOME/.bash_profile
+echo "export SEEDS=${SEEDS}" >> $HOME/.bash_profile
+
+source $HOME/.bash_profile
+
+sleep 1
+if [ ! $MONIKER ]; then
+	read -p "ENTER MONIKER NAME: " MONIKER
+	echo 'export MONIKER='$MONIKER >> $HOME/.bash_profile
+fi
+
+echo "30 installation_progress"
+
+# Go installation
+cd $HOME
+wget "https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$GO_VERSION.linux-amd64.tar.gz"
+rm "go$GO_VERSION.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+go version
+
+echo "60 installation_progress"
+
+sleep 1
+
+cd $HOME
+rm -rf $PROJECT_FOLDER
+git clone $REPO
+cd $PROJECT_FOLDER
+git checkout $VERSION
+make install
+
+sleep 1
+
+# Prepare binaries for Cosmovisor
+mkdir -p $HOME/${SYSTEM_FOLDER}/cosmovisor/genesis/bin
+mv $(which $EXECUTE) $HOME/${SYSTEM_FOLDER}/cosmovisor/genesis/bin/
+
+# Create application symlinks
+sudo ln -s $HOME/${SYSTEM_FOLDER}/cosmovisor/genesis $HOME/${SYSTEM_FOLDER}/cosmovisor/current -f
+sudo ln -s $HOME/${SYSTEM_FOLDER}/cosmovisor/current/bin/${EXECUTE} /usr/local/bin/${EXECUTE} -f
+
+# Download and install Cosmovisor
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
+
+# Create service
+sudo tee /etc/systemd/system/${EXECUTE}.service > /dev/null << EOF
+[Unit]
+Description=${EXECUTE}
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/${SYSTEM_FOLDER}"
+Environment="DAEMON_NAME=${EXECUTE}"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/${SYSTEM_FOLDER}/cosmovisor/current/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+$EXECUTE config chain-id $CHAIN_ID
+$EXECUTE config node tcp://localhost:${PORT}657
+$EXECUTE init $MONIKER --chain-id $CHAIN_ID
+
+# Set peers and seeds
+sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|" $HOME/$SYSTEM_FOLDER/config/config.toml
+sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/$SYSTEM_FOLDER/config/config.toml
+
+# Download genesis and addrbook
+curl -Ls $GENESIS_FILE > $HOME/$SYSTEM_FOLDER/config/genesis.json
+curl -Ls $ADDRBOOK > $HOME/$SYSTEM_FOLDER/config/addrbook.json
+
+echo "75 installation_progress"
+
+# Set Config Pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="10"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+
+
+# Set minimum gas price
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0025$DENOM\"/" $HOME/$SYSTEM_FOLDER/config/app.toml
+
+sleep 3
+
+#fast sync with snapshot
+SNAPSHOT=http://snapshots.autostake.com/lyIs25DaSWMSm8evWKHGQrb/regen-1/latest.tar.lz4
+cp $HOME/$SYSTEM_FOLDER/data/priv_validator_state.json $HOME/$SYSTEM_FOLDER/priv_validator_state.json.backup
+rm -rf $HOME/$SYSTEM_FOLDER/data/*
+mv $HOME/$SYSTEM_FOLDER/priv_validator_state.json.backup $HOME/$SYSTEM_FOLDER/data/priv_validator_state.json
+curl -L $SNAPSHOT | tar -I lz4 -xf - -C $HOME/$SYSTEM_FOLDER
+
+# Upgrade info
+[[ -f $HOME/$SYSTEM_FOLDER/data/upgrade-info.json ]] && cp $HOME/$SYSTEM_FOLDER/data/upgrade-info.json $HOME/$SYSTEM_FOLDER/cosmovisor/genesis/upgrade-info.json
+
+sudo systemctl daemon-reload
+sudo systemctl enable $EXECUTE
+sudo systemctl restart $EXECUTE
+
+echo "export NODE_PROPERLY_INSTALLED=true" >> $HOME/.bash_profile
+
+echo '=============== SETUP IS FINISHED ==================='
+echo -e "CHECK OUT YOUR LOGS : \e[1m\e[32mjournalctl -fu ${EXECUTE} -o cat\e[0m"
+echo -e "CHECK SYNC: \e[1m\e[32mcurl -s localhost:${PORT}657/status | jq .result.sync_info\e[0m"
+source $HOME/.bash_profile
+
+rm -- "$0"
